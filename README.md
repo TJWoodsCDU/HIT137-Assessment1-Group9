aaf62ae300de4dc3d9c9a047649e24926b81cc24
TJWoodsCDU <S402983@students.cdu.edu.au>
2026-07-31 21:35:34 +0800

    Structure formatting

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # HIT137 Software Now Group 9 Assessment 1
+
+To enter text, please paste it into the file `input.txt`
+
+The analysis report will show up in output.txt
---
+++ main.py
@@ -0,0 +1,59 @@
+"""
+Group Name: Group 9
+
+Group Members:
+
+Janelle Dobunaba - ######
+Kien Nguyen - ######
+TJ Woods - 402983
+Andrew Yang - ######
+
+See README.md for how to input and where to find output
+"""
+
+
+def task1(text) -> str:
+    """Character Census"""
+    report = ""
+
+    return report
+
+
+def task2(text) -> str:
+    """Case and Vowel Breakdown"""
+    report = ""
+
+    return report
+
+
+def task3(text) -> str:
+    """Word Statistics"""
+    report = ""
+
+    return report
+
+
+def task4(text) -> str:
+    """Line and Sentence Analysis"""
+    report = ""
+
+    return report
+
+
+if __name__ == "__main__":
+    # Get input
+    with open("input.txt", "r") as inpt:
+        text = inpt.read()
+
+    # Create Report
+    report = ""
+
+    report += task1(text) + "\n----------------------\n"
+    report += task2(text) + "\n----------------------\n"
+    report += task3(text) + "\n----------------------\n"
+    report += task4(text) + "\n----------------------\n"
+
+    # Write Report
+    with open("output.txt", "w") as outpt:
+        outpt.write(report)
+    print("Report written into `output.txt`")
